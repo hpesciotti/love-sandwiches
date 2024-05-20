14ff72c0538c09ec770d2717cafc9c55366ddfd6
Henrique Pesciotti <hpesciotti@gmail.com>
2024-05-20 15:48:40 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+# from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -29,7 +29,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n") #Got to insert the line break symbol for it to work properly
 
         sales_data = data_str.split(",")
         validate_data(sales_data)
